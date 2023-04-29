b67d3cece878dd0f7d7a6d61d651811ba50c9412
TRHorse <therunninghorse00@gmail.com>
2023-04-30 01:42:52 +0600

    some small changes in the simple jwt config

--- heychats_backend/settings.py
+++ heychats_backend/settings.py
@@ -118,9 +118,10 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'user.User'
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 # Internationalization
