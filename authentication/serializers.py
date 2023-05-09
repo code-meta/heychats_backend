14a6e9afe6487076d6b812faef49cd804dd3297e
TRHorse <therunninghorse00@gmail.com>
2023-05-10 02:37:56 +0600

    about field is added in user info serializer.

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -26,7 +26,7 @@ class CreateUserSerialzier(serializers.ModelSerializer):
 class CommonUserInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "profile", "connection_id"]
+        fields = ["id", "username", "email", "profile", "connection_id", "about"]
 
 
 class UploadProfileSerializer(serializers.ModelSerializer):
